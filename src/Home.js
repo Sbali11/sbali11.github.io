f1f43dea888ffa046dabdeff6368e0ec71bc1f58
Shreya <sbali@andrew.cmu.edu>
2021-05-08 17:06:59 +0530

    changes to spotify position

--- src/Home.js
+++ src/Home.js
@@ -13,6 +13,7 @@ export default function Home() {
         "Opinionat-ing": 4
     }
     const [isClicked, setIsClicked] = useState([false, false, false, false, false]);
+    const [count, setCount] = useState([0, 0, 0, 0, 0]);
 
     const [interested, setInterested] = useState(new Set());
     function toggleElem(item_key, name) {
@@ -24,12 +25,19 @@ export default function Home() {
 
         } else {
             x.style.color = '#575395';
+
         }
         const newList = isClicked.map((data, key) => {
             if (key == item_key) return !isClicked[key];
             else return isClicked[key]
         })
+
+        const newCount = count.map((data, key) => {
+            if (key == item_key && !isClicked[key]) return count[key] + 1;
+            else return count[key]
+        })
         setIsClicked(newList);
+        setCount(newCount)
 
 
 
@@ -49,27 +57,26 @@ export default function Home() {
             {/*/container*/}
             {/* Header Image */}
             <section >
-                <div className="parallax">
+                <div className="parallax" height={100}>
                     {/* Slider Button (don't edit!)*/}
 
                     {/* HEADER HEADLINE */}
 
-                    <div className="col-pd-5">
-                        <div style={{position: "absolute", left:"100px", top: "100px"}}>
-                        <h1 className="animated fadeInDown">
-                            <span style={{ color: '#6512b2' }}>S</span>hreya<br />Bali
-                        </h1>{/* Title */}
-                        <Button type="Button" className="btn btn-social-icon btn-github m-1" href='https://github.com/Sbali11'><i className="fa fa-github" /></Button>
-                        <Button type="Button" className="btn btn-social-icon btn-twitter m-1" href='https://twitter.com/ShreyaBali3'><i className="fa fa-twitter" /></Button>
-                        <Button type="Button" className="btn btn-social-icon btn-linkedin m-1" href='https://linkedin.com/in/shreya-bali-089478107'><i className="fa fa-linkedin" /></Button>
-                        </div>
+                    <div className="d-flex justify-content-center">
 
-                        <div id="stay-at-bottom">
-                            <iframe src="https://open.spotify.com/embed/playlist/66phfa44ZZyEQw5JDvyDGe" width={300} height={350} frameBorder={0} allowTransparency="true" allow="encrypted-media" />
-
+                        <div className="align-self-center">
+                            <h1 className="animated fadeInDown">
+                                <span style={{ color: '#6512b2' }}>S</span>hreya<br />Bali
+                            </h1>
+                            <Button type="Button" className="btn btn-social-icon btn-github m-1" href='https://github.com/Sbali11'><i className="fa fa-github" /></Button>
+                            <Button type="Button" className="btn btn-social-icon btn-twitter m-1" href='https://twitter.com/ShreyaBali3'><i className="fa fa-twitter" /></Button>
+                            <Button type="Button" className="btn btn-social-icon btn-linkedin m-1" href='https://linkedin.com/in/shreya-bali-089478107'><i className="fa fa-linkedin" /></Button>
                         </div>
                     </div>
+
                 </div>
+
+
             </section>
             {/* Header Image End */}
         </header>
@@ -80,6 +87,7 @@ export default function Home() {
   ABOUT SECTION  
   =========================== */}
         <section className="intro text-center section-padding color-bg" id="about">
+
             <div className="container">
                 {/* WELCOME TEXT */}
                 <div className="row">
@@ -93,14 +101,16 @@ export default function Home() {
                                 computer science can be used to make life better.
             </b></p>
                         <p />
-                        <p id="demo" />
+                        <div class="mt-auto p-2 bd-highlight">
+                            <iframe src="https://open.spotify.com/embed/playlist/66phfa44ZZyEQw5JDvyDGe" width={300} height={200} frameBorder={0} allowTransparency="true" allow="encrypted-media" />
+                        </div>
                     </div>
                 </div>
             </div>
         </section>
         <section className="intro text-center section-padding color-purple" id="about">
             <div>
-                <a href="/src/files/Resume_Shreya_Bali.pdf" download> My Resume </a>
+                <a href="https://drive.google.com/file/d/1yPI_9m8-c4YRtmTSp98fT2fBYDNt6vj8/view?usp=sharing" download> My Resume </a>
             </div>
         </section>
         {/* ==========================
@@ -121,7 +131,9 @@ export default function Home() {
                         <div className="services">
                             {/* Service Box 1 */}
                             <div className="col-md-4  item">
+
                                 <div className="icon" id="coding" onClick={() => toggleElem(0, "coding")}>
+
                                     <i className="fa fa-bar-chart" />{/* Icon */}
                                 </div>
                                 <h2>Coding</h2>{/* Title */}
