d9262d5030df2d08b3a57341f8970ba2443235c8
Shreya Bali <shreyabali.cs@gmail.com>
2024-09-21 13:43:23 -0400

    correct text

--- src/data/news.js
+++ src/data/news.js
@@ -1,7 +1,7 @@
 // news.js
 const news = [
     { date: 'August 2024', content: 'Started PhD in Human-Computer Interaction at Carnegie Mellon University!' },
-    { date: 'April 2023', content: 'Our Paper "Nooks: Social Spaces to Lower Hesitations in Interacting with New People at Work." was awared CHI 2023 Best Paper Honorable Mention at CHI 2023 ' },
+    { date: 'April 2023', content: 'Our Paper "Nooks: Social Spaces to Lower Hesitations in Interacting with New People at Work." was awared a Best Paper Honorable Mention at CHI 2023 ' },
     { date: 'September 2022', content: 'Joined the Anti-Abuse Team at Databricks!' },
     { date: 'June 2022', content: 'Defended my Master\'s Thesis on "Tools to facilitate working on Machine Learning in the Industry"' },
     { date: 'July 2021', content: 'Presented at the ICML Climate Change Workshop on "Prediction of Boreal Peatland Fires in Canada using Spatio-Temporal Methods," showcasing disaster prediction using advanced ML techniques.' },
